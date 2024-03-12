f9e43decec65e912c481aee99ed3339798b3509c
Rahul Dey <rahuldey832000@gmail.com>
2024-03-12 21:23:49 +0530

    Add aceternity ui and design navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-medium">Welcome to Music</h1>
+      <h1 className="text-3xl font-medium text-rose-300">Welcome to Music</h1>
     </main>
   );
 }
