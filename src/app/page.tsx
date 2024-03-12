883a2cffcdf9d64bc056dda992400894caab8742
Rahul Dey <rahuldey832000@gmail.com>
2024-03-12 21:33:58 +0530

    Fixed navbar hover effect issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-medium text-rose-300">Welcome to Music</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white[0.02]">
+      <h1 className="text-3xl font-medium text-rose-300 text-center">
+        Welcome to Music
+      </h1>
     </main>
   );
 }
